6a07fe4359b6a59f7d7ade85ee0de064d36f0ae7
croor005 <richard.crook@disney.com>
2020-08-03 16:28:35 -0700

    Trying secrets

--- cypress/integration/amazon.spec.js
+++ cypress/integration/amazon.spec.js
@@ -3,13 +3,13 @@ describe('Searching FAANG', () => {
     it('Amazon', () => {
 
         cy.visit("http://www.amazon.com")
-        cy.get('#twotabsearchtextbox').type('Star Trek T-shirts').type('{enter}')
-        cy.get('.a-size-mini > a > span').then(($lis) => {
+        // cy.get('#twotabsearchtextbox').type('Star Trek T-shirts').type('{enter}')
+        // cy.get('.a-size-mini > a > span').then(($lis) => {
 
             for (let i = 0; i < $lis.length; i++) {
                // expect($lis.eq(i).text()).to.match(/Star\sTrek|Enterprise|Federation|Picard/)
             }
-            cy.log(  "${{secrets.HELLO_TEST}}")
-        })
+            cy.log( "Secrets: " + process.env.HELLO_TEST)
+       // })
     })
 })
