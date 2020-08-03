eeb0f978fd94c909a1b309cce24155f5bda89f52
croor005 <richard.crook@disney.com>
2020-08-03 11:33:42 -0700

    First commit

---
+++ cypress/integration/amazon.spec.js
@@ -0,0 +1,15 @@
+describe('Searching FAANG', () => {
+
+    it('Amazon', () => {
+
+        cy.visit("http://www.amazon.com")
+        cy.get('#twotabsearchtextbox').type('Star Trek T-shirts').type('{enter}')
+        cy.get('.a-size-mini > a > span').then(($lis) => {
+
+            for (let i = 0; i < $lis.length; i++) {
+                expect($lis.eq(i).text()).to.match(/Star\sTrek|Enterprise|Federation|Picard/)
+            }
+
+        })
+    })
+})
