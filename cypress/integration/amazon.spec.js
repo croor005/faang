66388f0a47b3d4b88148c4904691875111462d23
croor005 <richard.crook@disney.com>
2020-08-03 17:11:29 -0700

    Trying secrets

--- cypress/integration/amazon.spec.js
+++ cypress/integration/amazon.spec.js
@@ -5,12 +5,12 @@ describe('Searching FAANG', () => {
     it('Amazon', () => {
 
         cy
-            .readFile('/Users/croor005/Webstorm/faang/.github/workflows/End_To_End_tests.yml')
+            .readFile('/Users/croor005/Webstorm/faang/.github/workflows/special.yaml')
             .then((str) => {
                 // parse the string into object literal
                 const data = YAML.parse(str)
 
-                cy.log(data.HELLO_TEST)
+                cy.log(data)
             })
     })
 })
