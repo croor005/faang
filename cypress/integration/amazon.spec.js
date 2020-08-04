7e46249ccfea36893f9f3d3ee59550a30527c5c3
croor005 <richard.crook@disney.com>
2020-08-03 17:36:19 -0700

    Trying secrets

--- cypress/integration/amazon.spec.js
+++ cypress/integration/amazon.spec.js
@@ -1,16 +1,8 @@
 describe('Searching FAANG', () => {
 
-    const YAML = require('yamljs')
 
     it('Amazon', () => {
 
-        cy
-            .readFile('.github/workflows/special.yaml')
-            .then((str) => {
-                // parse the string into object literal
-                const data = YAML.parse(str)
-
-                cy.log(data)
-            })
+        Cypress.env('HELLO_TEST')
     })
 })
